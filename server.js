2bb15ad50c564a937d0275516fb5dc9ad4cfb468
Vikrant Varma <vikrant.varma94@gmail.com>
2014-04-18 19:31:28 +0530

    Add Nodetime monitoring

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+if(process.env.NODETIME_ACCOUNT_KEY) {
+  require('nodetime').profile({
+    accountKey: process.env.NODETIME_ACCOUNT_KEY,
+    appName: process.env.APP_NAME
+  });
+}
+
 var restify = require('restify');
 var _ = require('underscore');
 var bunyan = require('bunyan');
